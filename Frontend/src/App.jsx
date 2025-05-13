5d11a729f18a6c469e7dfdb6f3f647cd10a58df7
Swabirah Iffat <aswabirah1249@gmail.com>
2025-05-13 17:29:59 +0600

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavigationBar from './components/navbar/Navigationbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <NavigationBar/>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/navbar/Navigationbar.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
+import './NavigationBar.css'
+
+const NavigationBar = () => {
+
+  const [menu, setMenu] = useState("home")
+
+  return (
+    <div className='navbar'>
+        <img src={assets.Logo1} alt="" className="logo" />
+        <ul className="navbar-menu">
+            <li className={menu==="home"?"active":""}>Home</li>
+            <li className={menu=== "menu"?"active":""}>Menu</li>
+            <li className={menu=== "mobile-app"?"active":""}>Mobile App</li>
+            <li className={menu=== "contact-us"?"active":""}>Contact Us</li>
+        </ul>
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt="" />
+            <div className="navbar-search-icon">
+                <img src={assets.basket_icon} alt="" />
+                <div className="dot">
+                </div>
+            </div>
+            <button>Sign In</button>
+        </div>
+    </div>
+  )
+}
+
+export default NavigationBar
